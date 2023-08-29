e9258d553a60d22eb1581fcf3ba2993435780065
fraan94 <frank.9406@hotmail.com>
2023-08-29 14:18:55 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
+//const container = document.querySelector("div");
+//const button = document.querySelector("button")
+
 document.addEventListener("DOMContentLoaded", function(){
+    document.querySelector("button").addEventListener("click",function(event){
+        event.stopPropagation();
+        alert("Hola!")})
     document.querySelector("div").addEventListener("click",function(){
-        alert("Hola soy el div!")
-    })})
-    
+        alert("Hola soy el div!")})
+        })
